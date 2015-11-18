d12d1d5791e9beb91c42b2e8ffd4e5224126c03a
Colin Hill <chill@iofficecorp.com>
2015-11-18 12:13:16 -0600

    Added javascript interface

---
+++ www/printer.js
@@ -0,0 +1,24 @@
+function print() {
+}
+print.prototype.getPrinter = function(successCallback, errorCallback) {
+  cordova.exec(successCallback,
+            errorCallback,
+            'CordovaPrinter',
+            'cordovaGetPrinter');
+};
+print.prototype.print = function(successCallback, errorCallback, serialNumber, labelData) {
+  cordova.exec(successCallback,
+             errorCallback,
+             'CordovaPrinter',
+             'cordovaPrint', [labelData, serialNumber]);
+};
+
+print.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.print = new print();
+  return window.plugins.print;
+};
+cordova.addConstructor(print.install);
